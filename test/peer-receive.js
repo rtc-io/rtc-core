fba17fa4c2b56c8510e18506df8a283881e24662
Damon Oehlman <damon.oehlman@gmail.com>
2013-07-05 14:32:40 +1000

    Enough receive tests for now

--- test/peer-receive.js
+++ test/peer-receive.js
@@ -1,17 +1,19 @@
 var test = require('tape'),
 	pull = require('pull-stream'),
 	pushable = require('pull-pushable'),
-	Peer = require('../peer'),
-	peer;
+	Peer = require('../peer');
 
 test('create a new peer', function(t) {
+	var peer;
+
 	t.plan(1);
 	peer = new Peer();
 	t.ok(peer.id, 'new peer created');
 });
 
 test('can send messages to the peer', function(t) {
-	var messages = pushable();
+	var messages = pushable(),
+		peer = new Peer();
 
 	t.plan(1);
 
@@ -25,4 +27,23 @@ test('can send messages to the peer', function(t) {
 	});
 
 	messages.push('hello');
+});
+
+test('triggers a disconnect event when the incoming stream ends', function(t) {
+	var messages = pushable(),
+		peer = new Peer();
+
+	t.plan(1);
+
+	pull(
+		messages,
+		peer.inbound()
+	);
+
+	peer.on('disconnect', function() {
+		t.pass('received a disconnect event');
+	});
+
+	messages.push('hello');
+	messages.end();
 });
