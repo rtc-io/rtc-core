480e03518ac5b3047378ddca4185e0ca889a4f07
Damon Oehlman <damon.oehlman@gmail.com>
2013-07-26 12:31:35 +1000

    Doc updates

--- errorcodes.js
+++ errorcodes.js
@@ -3,12 +3,9 @@
 'use strict';
 
 /**
-# rtc.io errorcodes
+## Core Error Codes
+**/
 
-__NOTE:__ Error codes listed here cannot be changed.  Once an errorcode
-has been allocated a value, then any changes could cause major 
-problems between client and server components in the rtc.io stack.
-*/
 // create the codes array
 var codes = module.exports = function() {
   return Object.keys(codes).map(function(longCode) {
@@ -18,15 +15,37 @@ var codes = module.exports = function() {
   });
 };
 
+/*
+__NOTE:__ Error codes listed here cannot be changed.  Once an errorcode
+has been allocated a value, then any changes could cause major 
+problems between client and server components in the rtc.io stack.
+*/
 var messages = {
   HSIP: 'An existing handshake is already in progress',
   HSSIM: 'A handshake process has been initiated by the target endpoint',
   REQCHAN: 'A channel is required to initiate a peer connection'
 };
 
-// initialise the individual error codes
+/**
+### HSIP
+
+Handshake in Progress.  
+**/
 codes.HANDSHAKE_IN_PROGRESS = 'HSIP';
+
+/**
+### HSSIM
+
+Simultaneous Dial.
+*/
 codes.SIMULTANEOUS_DIAL = 'HSSIM';
+
+/**
+### REQCHAN
+
+This error condition is encountered when a peer connection is used without
+being assigned a valid signalling channel to start with.
+**/
 codes.REQUIRE_CHANNEL = 'REQCHAN';
 
 codes.toError = function(code) {
