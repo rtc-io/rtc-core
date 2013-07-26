3317b8172b8de479c60ffc1863324f79eac3d775
Damon Oehlman <damon.oehlman@gmail.com>
2013-07-26 16:46:06 +1000

    Added detection functionality into core (moved from rtc)

---
+++ detect.js
@@ -0,0 +1,52 @@
+
+/* global window: false */
+
+'use strict';
+
+/**
+## rtc-core/detect
+
+A browser detection helper for accessing prefix-free versions of the various
+WebRTC types. 
+
+### Example Usage
+
+If you wanted to get the native `RTCPeerConnection` prototype in any browser
+you could do the following:
+
+```js
+var detect = require('rtc-core/detect'); // also available in rtc/detect
+var RTCPeerConnection = detect('RTCPeerConnection');
+```
+
+This would provide whatever the browser prefixed version of the
+RTCPeerConnection is available (`webkitRTCPeerConnection`, 
+`mozRTCPeerConnection`, etc).
+**/
+module.exports = function(target, prefixes) {
+  var prefixIdx;
+  var prefix;
+  var testName;
+  var hostObject = this || window;
+
+  // initialise to default prefixes 
+  // (reverse order as we use a decrementing for loop)
+  prefixes = (prefixes || ['ms', 'o', 'moz', 'webkit']).concat('');
+
+  // iterate through the prefixes and return the class if found in global
+  for (prefixIdx = prefixes.length; prefixIdx--; ) {
+    prefix = prefixes[prefixIdx];
+
+    // construct the test class name
+    // if we have a prefix ensure the target has an uppercase first character
+    // such that a test for getUserMedia would result in a 
+    // search for webkitGetUserMedia
+    testName = prefix + (prefix ?
+                            target.charAt(0).toUpperCase() + target.slice(1) :
+                            target);
+
+    if (typeof hostObject[testName] == 'function') {
+      return hostObject[testName];
+    }
+  }
+};
