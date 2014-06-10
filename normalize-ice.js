8f73073513e5b7b138abba29aaa72ba38d796165
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-10 12:42:27 +1000

    Initial implementation of normalize-ice

--- normalize-ice.js
+++ normalize-ice.js
@@ -1,5 +1,27 @@
 var detect = require('./detect');
+var match = require('./match');
+var url = require('url');
+
+function useNewFormat() {
+  return match('firefox') || match('chrome', '>=36');
+}
 
 module.exports = function(server) {
+  var uri;
+  var auth;
+
+  // if we have a url parameter parse it
+  if (server && server.url && useNewFormat()) {
+    uri = url.parse(server.url);
+    auth = (uri.auth || '').split(':');
+
+    return {
+      url: uri.protocol + uri.host,
+      urls: [ uri.protocol + uri.host ],
+      username: auth[0],
+      credential: auth[1]
+    };
+  }
+
   return server;
 };
