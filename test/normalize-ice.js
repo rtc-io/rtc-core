aaeb100f53b23dab01d5af771223c97de0c04667
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-10 13:08:06 +1000

    More comprehensive tests to ensure that the ?transport= strings for a turn url are preserved

--- test/normalize-ice.js
+++ test/normalize-ice.js
@@ -8,9 +8,55 @@ var stunFormatA = [
 ];
 
 var turnFormatA = [
-  { url: 'turn:tmp:test@hoth.rtc.io:3478' },
-  { url: 'turn:tmp:test@hoth.rtc.io' }
+  { url: 'turn:tmp:test@example.org:3478' },
+  { url: 'turn:tmp@example.org:3478', credential: 'test' },
+  { url: 'turn:tmp@example.org', credential: 'test' },
+  { url: 'turn:tmp@example.org?transport=tcp', credential: 'test' }
 ];
+
+test('normalizing turn url with embedded username:credential', function(t) {
+  var server;
+
+  t.plan(5);
+  t.ok(server = normalize(turnFormatA[0]));
+  t.equal(server.url, 'turn:example.org:3478');
+  t.deepEqual(server.urls, ['turn:example.org:3478']);
+  t.equal(server.username, 'tmp');
+  t.equal(server.credential, 'test');
+});
+
+test('normalizing turn url with embedded username', function(t) {
+  var server;
+
+  t.plan(5);
+  t.ok(server = normalize(turnFormatA[1]));
+  t.equal(server.url, 'turn:example.org:3478');
+  t.deepEqual(server.urls, ['turn:example.org:3478']);
+  t.equal(server.username, 'tmp');
+  t.equal(server.credential, 'test');
+});
+
+test('normalizing turn url with embedded username (no port)', function(t) {
+  var server;
+
+  t.plan(5);
+  t.ok(server = normalize(turnFormatA[2]));
+  t.equal(server.url, 'turn:example.org');
+  t.deepEqual(server.urls, ['turn:example.org']);
+  t.equal(server.username, 'tmp');
+  t.equal(server.credential, 'test');
+});
+
+test('normalizing turn url with embedded username (transport specified)', function(t) {
+  var server;
+
+  t.plan(5);
+  t.ok(server = normalize(turnFormatA[3]));
+  t.equal(server.url, 'turn:example.org?transport=tcp');
+  t.deepEqual(server.urls, ['turn:example.org?transport=tcp']);
+  t.equal(server.username, 'tmp');
+  t.equal(server.credential, 'test');
+});
 
 test('can detect an RTCPeerConnection constructor', function(t) {
   t.plan(1);
