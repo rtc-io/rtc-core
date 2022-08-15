7bd08bb1c3763e0d8e548290cfdafe077305cc9e
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2022-08-15 14:26:42 +1000

    Export os (#4)
    Co-authored-by: Buddhika Jayawardhana <buddhika@coviu.com>

--- detect.js
+++ detect.js
@@ -73,3 +73,4 @@ detect.moz = typeof navigator != 'undefined' && !!navigator.mozGetUserMedia;
 // set the browser and browser version
 detect.browser = browser.name;
 detect.browserVersion = detect.version = browser.version;
+detect.os = browser.os;
